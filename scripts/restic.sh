430ce669365e7a49168bb240ce371904163db4af
Fritz Duchardt <fritz@duchardt.net>
2025-10-12 10:58:12 +0200

    fix: store all backups in the same bucket

--- scripts/restic.sh
+++ scripts/restic.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 url="s3.eu-central-3.ionoscloud.com"
-bucket_name="friclu-immich"
-key_id="${1:?provide aws access key id}"
-secret="${2:?provide aws access secret key}"
-password="${3:?provide restic password}"
+bucket_name="${1:?provide bucket name}"
+key_id="${2:?provide aws access key id}"
+secret="${3:?provide aws access secret key}"
+password="${4:?provide restic password}"
+shift 4
 
 export AWS_ACCESS_KEY_ID="$key_id"
 export AWS_SECRET_ACCESS_KEY="$secret"
 export RESTIC_PASSWORD="$password"
 
-restic -r "s3:https://$url/$bucket_name" snapshots
+restic -r "s3:https://$url/$bucket_name" "${@}"
